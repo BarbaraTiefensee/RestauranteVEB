c96c214d418e096ef6702cc134e28d193b629814
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2020-03-06 14:34:33 -0300

    Criação das entidade.

---
+++ DTO/ComplexTypes/Endereco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO.ComplexTypes
+{
+    public class Endereco
+    {
+        public string CEP { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+    }
+}
---
+++ DTO/Enums/Bebidas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO.Enums
+{
+    public enum Bebidas
+    {
+
+    }
+}
