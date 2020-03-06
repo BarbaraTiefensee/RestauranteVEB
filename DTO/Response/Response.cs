fa5bc08a32a986a40aa671d64d56da27ac7090ce
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2020-03-06 15:10:38 -0300

    Integração dos responses.

---
+++ DTO/Response/Response.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class Response
+    {
+        public bool Sucesso { get; set; }
+
+        public List<string> Erros { get; set; }
+
+        public string GetErrorMessage()
+        {
+            StringBuilder builder = new StringBuilder();
+            //Lambda Expression
+            Erros.ForEach(erro => builder.AppendLine(erro));
+            return builder.ToString();
+        }
+
+        public bool HasErrors()
+        {
+            return this.Erros.Count > 0;
+        }
+
+        public Response()
+        {
+            this.Erros = new List<string>();
+        }
+    }
