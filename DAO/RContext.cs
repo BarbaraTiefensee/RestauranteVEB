ed319f1b92455543cd38e8471f560f23dc46b08d
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2020-03-06 14:59:29 -0300

    Definição da estrutura dos dados.

---
+++ DAO/RContext.cs
@@ -0,0 +1,36 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Reflection;
+using System.Text;
+
+namespace DAO
+{
+    class RContext : DbContext
+    {
+        public RContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\900208\Documents\SSLocadora.mdf;Integrated Security=True;Connect Timeout=30")
+        {
+
+        }
+
+        public DbSet<EstoqueDTO> Estoques { get; set; }
+        public DbSet<IngredienteDTO> Ingredientes { get; set; }
+        public DbSet<PedidoDTO> Pedidos { get; set; }
+        public DbSet<UsuarioDTO> Usuarios { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Properties()
+                .Where(c => c.PropertyType == typeof(string))
+                .Configure(c => c.IsRequired().IsUnicode(false));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
