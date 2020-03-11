6add7d7cfe331bbf45eca0dba4ff4040143ac88a
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2020-03-11 14:14:17 -0300

    Correções.

--- DTO/UsuarioDTO.cs
+++ DTO/UsuarioDTO.cs
@@ -10,9 +10,9 @@ namespace DTO
     {
         public int ID { get; set; }
         public string Nome { get; set; }
+        public string CPF { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        public string CPF { get; set; }
         public Permissao Permissao { get; set; }
     }
 }
