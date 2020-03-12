22ff0b643a589f3b2f712461aca53f21d5289e2f
Eduardo Zortea <eduardozortea9@hotmail.com>
2020-03-12 16:04:22 -0300

    vai da pau

--- DTO/BebidaDTO.cs
+++ DTO/BebidaDTO.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DTO
 {
     public class BebidaDTO
     {
+        
+
         public int ID { get; set; }
         public string Nome { get; set; }
         public double Preco { get; set; }
