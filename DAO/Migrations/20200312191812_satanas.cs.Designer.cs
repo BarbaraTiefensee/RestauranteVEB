a338410ba45f1740fe0b2ffe90c46ead285703df
Eduardo Zortea <eduardozortea9@hotmail.com>
2020-03-12 16:31:25 -0300

    cookies feitos amem tomara que de bom

--- DAO/Migrations/20200312191812_satanas.cs.Designer.cs
+++ DAO/Migrations/20200312191812_satanas.cs.Designer.cs
@@ -3,14 +3,16 @@ using DAO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAO.Migrations
 {
     [DbContext(typeof(RContext))]
-    partial class RContextModelSnapshot : ModelSnapshot
+    [Migration("20200312191812_satanas.cs")]
+    partial class satanascs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
